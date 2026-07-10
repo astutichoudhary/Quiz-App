314492a7e5eb5ca98f82ded63398d67d74015f37
astutichoudhary <astutichoudhary31@gmail.com>
2026-07-10 21:06:18 +0530

    initial code of quiz app

--- quizapp.py
+++ quizapp.py
@@ -1,1 +1,16 @@
-# code upcoming.
+from tkinter import *
+
+window = Tk()
+
+window.geometry('720x480')
+window.title("Quiz App.")
+window.config(background="#E99FBF")
+
+quiztitle = Label(window, text= "Quiz app",
+                  font = ("Georgia",25,'italic'),
+                  padx=5,pady=5
+                  )
+quiztitle.pack(anchor='center')
+
+
+window.mainloop()
